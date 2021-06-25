f3ae0f3319c6e19713da9955fe7a65b0c3ab38b0
Ernie <ernestjohnson80@yahoo.com>
2021-06-25 13:34:04 -0700

    Update index.jjs 😃

--- dist/index.js
+++ dist/index.js
@@ -1,7 +1,7 @@
 // modules are defined as an array
 // [ module function, map of requires ]
 //
-// map of requires is short require name -> numeric require
+// map of requires is short require -> numeric require
 //
 // anything defined in a previous bundle is accessed via the
 // orig method which is the require for previous bundles
